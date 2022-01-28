3bc935a133550d6856235058d83a390ce6f9918b
harshil09 <harshilsoni757@gmail.com>
2022-01-27 21:39:29 -0600

    debug

--- Website/auth.py
+++ Website/auth.py
@@ -1,10 +1,26 @@
-from flask import Blueprint, render_template, request, flash
+from .__init__ import create_app
+from flask import Blueprint, render_template, request, flash, jsonify
+#from markupsafe import re
+from flask_pymongo import PyMongo
+from .models import postData, validatelogin
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods= ['GET','POST'])
 def login():
-    return render_template("login.html", methods=['GET'])
+  if request.method == 'POST':
+      email = request.form.get('email')
+      password = request.form.get('password ')
+      flask_obj = create_app()
+      Mongo = PyMongo(flask_obj)
+      currentcollection = Mongo.db.Banking
+
+      if currentcollection.find_one({'email': email}) and currentcollection.find_one({'password1': password}):
+          return jsonify({'email': email, 'password1': password})
+      else:
+          return jsonify({'error': 'cant find'})
+   
+  return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
@@ -14,21 +30,32 @@ def logout():
 def signup():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        firstname = request.form.get('firstname')
+        lastname = request.form.get('lastname')
+        phonenumber = request.form.get('phonenumber')
+        DOB = request.form.get('DOB')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
+
         if len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
-        elif len(firstName) < 2:
+        elif len(firstname) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif len(lastname) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif len(phonenumber) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif len(DOB) < 2:
             flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
-            flash('Passwords don\'t match.', category='error')
+            flash('Passwords dont match.', category='error')
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
             flash('Account created!', category='success')
-            
 
+       
+        return postData(firstname, lastname, phonenumber, DOB, email, password1)
 
     return render_template("sign_up.html")
